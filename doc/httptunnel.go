febeb848f230f175c0c0223a726f8f03986de238
idk <idk@idk.local>
2019-03-05 16:09:46 -0500

    add the tutorial

--- doc/httptunnel.go
+++ doc/httptunnel.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"crypto/tls"
+	"flag"
 	"io"
 	"log"
 	"net/http"
 	"strings"
 	"time"
-    "flag"
 )
-
 
 // tutorial line 48
 import (
@@ -43,7 +42,7 @@ func delHopHeaders(header http.Header) {
 	for _, h := range hopHeaders {
 		header.Del(h)
 	}
-    // tutorial line 117
+	// tutorial line 117
 	if header.Get("User-Agent") != "MYOB/6.66 (AN/ON)" {
 		header.Set("User-Agent", "MYOB/6.66 (AN/ON)")
 	}
@@ -58,16 +57,16 @@ type Proxy struct {
 // NewClient is on 181, 246
 func (p *Proxy) NewClient() *http.Client {
 	return &http.Client{
-        // tutorial line 187
+		// tutorial line 187
 		Transport: &http.Transport{
-			DialContext:           p.Sam.DialContext,
-            //tutorial line 195
-            MaxConnsPerHost:       1,
-            MaxIdleConns:          0,
-            MaxIdleConnsPerHost:   1,
-            DisableKeepAlives:     false,
-            ResponseHeaderTimeout: time.Second * 600,
-            IdleConnTimeout:       time.Second * 300,
+			DialContext: p.Sam.DialContext,
+			//tutorial line 195
+			MaxConnsPerHost:       1,
+			MaxIdleConns:          0,
+			MaxIdleConnsPerHost:   1,
+			DisableKeepAlives:     false,
+			ResponseHeaderTimeout: time.Second * 600,
+			IdleConnTimeout:       time.Second * 300,
 			TLSNextProto:          make(map[string]func(authority string, c *tls.Conn) http.RoundTripper),
 			TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
 		},
@@ -75,7 +74,6 @@ func (p *Proxy) NewClient() *http.Client {
 		Timeout:       time.Second * 600,
 	}
 }
-
 
 // ServeHTTP is on line 63, 126
 func (p *Proxy) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
@@ -88,7 +86,7 @@ func (p *Proxy) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-    // tutorial line 137
+	// tutorial line 137
 	delHopHeaders(req.Header)
 
 	p.get(wr, req)
@@ -97,7 +95,7 @@ func (p *Proxy) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
 // tutorial lines 147, 155
 func (p *Proxy) get(wr http.ResponseWriter, req *http.Request) {
 	req.RequestURI = ""
-    resp, err := p.Client.Do(req)
+	resp, err := p.Client.Do(req)
 	if err != nil {
 		log.Println("ServeHTTP:", err)
 		return
@@ -113,8 +111,7 @@ func main() {
 	var addr = flag.String("addr", "127.0.0.1:7950", "The addr of the application.")
 	flag.Parse()
 
-
-// tutorial line 71, 222
+	// tutorial line 71, 222
 	sam, err := goSam.NewClientFromOptions(
 		goSam.SetHost("127.0.0.1"),
 		goSam.SetPort("7656"),
@@ -134,10 +131,10 @@ func main() {
 	handler := &Proxy{
 		Sam: sam,
 	}
-    // tutorial line 245
-    handler.Client = handler.NewClient()
+	// tutorial line 245
+	handler.Client = handler.NewClient()
 
-    // tutorial line 252
+	// tutorial line 252
 	log.Println("Starting proxy server on", *addr)
 	if err := http.ListenAndServe(*addr, handler); err != nil {
 		log.Fatal("ListenAndServe:", err)
