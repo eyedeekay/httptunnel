d1ce4ed192acaa8f80c6c3f5011aff7a2f04c05f
idk <idk@idk.local>
2019-07-09 14:23:39 -0400

    readme for browserproxy, fix license

--- multiproxy/authenticate.go
+++ multiproxy/authenticate.go
@@ -9,11 +9,41 @@ import (
 	"strings"
 )
 
-// Create a struct that models the structure of a user, both in the request body, and in the DB
 type Credentials struct {
 	User string
 	Site string
 }
+
+// This part is copied directly from the Go source code https://golang.org/src/net/http/request.go?s=29249:29315#L872
+// https://golang.org/LICENSE
+/*
+Copyright (c) 2009 The Go Authors. All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+   * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+   * Neither the name of Google Inc. nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 func ProxyBasicAuth(r *http.Request) (username, password string, ok bool) {
 	auth := r.Header.Get("Proxy-Authorization")
@@ -22,7 +52,6 @@ func ProxyBasicAuth(r *http.Request) (username, password string, ok bool) {
 	}
 	return parseBasicAuth(auth)
 }
-
 func parseBasicAuth(auth string) (username, password string, ok bool) {
 	const prefix = "Basic "
 	// Case insensitive prefix match. See Issue 22736.
@@ -41,6 +70,8 @@ func parseBasicAuth(auth string) (username, password string, ok bool) {
 	return cs[:s], cs[s+1:], true
 
 }
+// End copied part
+
 func DecodeIdentity(body *http.Request) (*http.Request, *Credentials, error) {
 	var creds Credentials
 	bb, err := ioutil.ReadAll(body.Body)
